a8cd40e8fe2af96c38572703f92407829722db17
Romanov Andrei <romashka013@ya.ru>
2023-11-21 21:58:42 +0300

    Переписал с использованием f строк

--- 2023.11.16/4.py
+++ 2023.11.16/4.py
@@ -6,7 +6,8 @@ number = int(input('Введите любое трехзначное число:
 number_1 = number  // 100
 number_2 = number % 100 // 10
 number_3 = number % 10
-print('Сумма цифр = ', number_1 + number_2 + number_3, '\n' + 'Произведение цифр = ', number_1 * number_2 * number_3 )
+# print('Сумма цифр = ', number_1 + number_2 + number_3, '\n' + 'Произведение цифр = ', number_1 * number_2 * number_3 )
+print(f'Сумма цифр = {number_1 + number_2 + number_3}', f'Произведение цифр = {number_1 * number_2 * number_3}', sep = '\n')
 
 # Ввод: 
 
